4a0179e38da896425d9f5d3b62959eec430a550f
DavideLopez <davidelopez7@gmail.com>
2022-09-01 17:13:40 +0200

    inserita prima stampa mail

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log('mail list')
+
+const app = new Vue({
+    el: '#root',
+    data: {
+        randomMail: null,
+    },
+    mounted() {
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((res)  => {
+            console.log(res.data.response)
+            this.randomMail = res.data.response
+        })
+    },
+})
