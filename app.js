b3a6db0c1462f37e9834148a10dfd4db6d2eb30f
DavideLopez <davidelopez7@gmail.com>
2022-09-01 17:26:26 +0200

    10mail in console

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ console.log('mail list')
 const app = new Vue({
     el: '#root',
     data: {
-        randomMail: null,
+        randomMail: "",
         listRandomMail: []
     },
     mounted() {
         let i = 0;
         while (i < 10) {
-        axios
+          axios
             .get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((res)  => {
                 console.log(res.data.response)
                 this.randomMail = res.data.response
+                if (this.listRandomMail.includes(this.randomMail)) {
+                    this.listRandomMail.push(this.RandomMail)
+                }
             })
             i ++
         }
