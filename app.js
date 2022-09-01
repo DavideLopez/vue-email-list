da5917bd770ece61eb280418455d699b1fd88f14
DavideLopez <davidelopez7@gmail.com>
2022-09-01 17:21:58 +0200

    mail stampate in console

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ const app = new Vue({
     el: '#root',
     data: {
         randomMail: null,
+        listRandomMail: []
     },
     mounted() {
+        let i = 0;
+        while (i < 10) {
         axios
-        .get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((res)  => {
-            console.log(res.data.response)
-            this.randomMail = res.data.response
-        })
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((res)  => {
+                console.log(res.data.response)
+                this.randomMail = res.data.response
+            })
+            i ++
+        }
+        
     },
 })
